9256b6114f92eed322ff4555c0c979aa615df6c4
Tamal Saha <tamal@appscode.com>
2021-11-14 13:34:03 -0800

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	flag "github.com/spf13/pflag"
+)
 
+type FeatureFlags map[string]string
+
+func(f FeatureFlags) ToSlice() []string {
+	if len(f) == 0 {
+		return nil
+	}
+	result := make([]string, 0, len(f))
+	for k, v := range f {
+		result = append(result, fmt.Sprintf("%s=%s", k, v))
+	}
+	return result
+}
+
+func main() {
+	var fmap map[string]string
+	flag.StringToStringVar(&fmap, "feature-flag", fmap, "list of feature flags")
+	flag.Parse()
+
+	ff := FeatureFlags(fmap)
+	fmt.Println(ff.ToSlice())
 }
